79442a7b9d12ecbacba9fa7ee4f0de0ae4b78a9e
Christian Peters <christian.peters@zweitag.de>
2010-12-26 12:17:37 +0100

    factors out general init_component specs from block spec

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,5 +1,5 @@
 describe ActiveComponent::Base do
-  
+
   context "component initialization" do
     describe "html_class" do
       it "should include the hyphenized component title" do
@@ -8,14 +8,113 @@ describe ActiveComponent::Base do
         g.instance_eval {[html_class].flatten}.should be_include 'happy-greeting-box'
         d = Div.new
         d.title = "Happy div"
-        g.instance_eval {[html_class].flatten}.should be_include 'happy-div'
+        d.instance_eval {[html_class].flatten}.should be_include 'happy-div'
       end
-      it "should include the class_name unless it is an html tag wrapper", :focus => true do
+      it "should include the class_name unless it is an html tag wrapper" do
         g = GreetingBox.new
         g.instance_eval {[html_class].flatten}.should be_include 'greeting-box'
         d = Div.new
         d.instance_eval {[html_class].flatten}.should_not be_include 'div'
       end
+    end
+
+    class NewsWidget < ActiveComponent::Base
+      attr_accessor :author
+
+      def initialize(*args, &content_block)
+        init_component(args, [:content, :title, :author, :attributes], &content_block)
+      end
+    end
+
+    describe "init_component" do
+      context "custom component" do
+        before :all do
+          @news_content = "The pope claims that condoms are not necessarily evil. Where is this heading to?"
+          # TODO support for special character removal pending
+          #@news_title   = "Scandal in the Vatican!"
+          @news_title   = "Scandal in the Vatican"
+          @news_author  = "Karl Atzinger"
+          @news_attributes = {:class => 'gossip', :title => 'click to read whole article'}
+        end
+
+        it "should set all parameters correctly when given in order" do
+          n = NewsWidget.new @news_content, @news_title, @news_author, {:class => 'gossip'}
+          n.content.should    == @news_content
+          n.title.should      == @news_title
+          n.author.should     == @news_author
+          n.attributes.should == {:class => ['scandal-in-the-vatican', 'news-widget', 'gossip']}
+        end
+
+        it "should set all parameters correctly when given as key value pairs" do
+          n = NewsWidget.new :attributes => @news_attributes, :title => @news_title, :content => @news_content, :author => @news_author
+          n.content.should    == @news_content
+          n.title.should      == @news_title
+          n.author.should     == @news_author
+          n.attributes.should == {:title => @news_attributes[:title], :class => ['scandal-in-the-vatican', 'news-widget', 'gossip']}
+        end
+
+        it "should set all parameters correctly when given in a mixed way" do
+          n = NewsWidget.new @news_title, @news_author, :content => @news_content, :attributes => @news_attributes
+          n.content.should    == @news_content
+          n.title.should      == @news_title
+          n.author.should     == @news_author
+          n.attributes.should == {:title => @news_attributes[:title], :class => ['scandal-in-the-vatican', 'news-widget', 'gossip']}
+        end
+
+        it "should prioritize content blocks over content arguments" do
+          n = NewsWidget.new(:content => "ignorable") { @news_content }
+          n.content.should == @news_content
+        end
+
+        it "should fill @attributes with remaining arguments of the last hash" do
+          n = NewsWidget.new @news_title, :id => 'news', :content => @news_content, :class => @news_attributes[:class]
+          n.content.should    == @news_content
+          n.title.should      == @news_title
+          n.attributes.should == {:id => 'news', :class => ['scandal-in-the-vatican', 'news-widget', 'gossip']}
+        end
+      end
+
+      context "html wrapper component (grand-child of base)" do
+        before :all do
+          @bq_content     = "It is better to be quotable than to be honest."
+          @bq_title       = "Tom Stoppard on Quotations"
+          @bq_attributes  = {:class => 'quotes', :cites => "http://www.quotationspage.com/quote/368.html"}
+        end
+
+        it "should set all parameters correctly when given in order" do
+          bq = Blockquote.new @bq_content, @bq_title, @bq_author, @bq_attributes
+          bq.content.should    == @bq_content
+          bq.title.should      == @bq_title
+          bq.attributes.should == {:cites => @bq_attributes[:cites], :class => [@bq_title.hyphenize, 'quotes']}
+        end
+
+        it "should set all parameters correctly when given as key value pairs" do
+          bq = Blockquote.new :attributes => @bq_attributes, :title => @bq_title, :content => @bq_content
+          bq.content.should    == @bq_content
+          bq.title.should      == @bq_title
+          bq.attributes.should == {:cites => @bq_attributes[:cites], :class => [@bq_title.hyphenize, 'quotes']}
+        end
+
+        it "should set all parameters correctly when given in a mixed way" do
+          bq = Blockquote.new @bq_title, :content => @bq_content, :attributes => @bq_attributes
+          bq.content.should    == @bq_content
+          bq.title.should      == @bq_title
+          bq.attributes.should == {:cites => @bq_attributes[:cites], :class => [@bq_title.hyphenize, 'quotes']}
+        end
+
+        it "should prioritize content blocks over content arguments" do
+          bq = Blockquote.new(:content => "ignorable") { @bq_content }
+          bq.content.should    == @bq_content
+        end
+
+        it "should fill attributes with remaining arguments of the last hash" do
+          bq = Blockquote.new @bq_title, :cites => "http://www.quotationspage.com/quote/368.html", :content => @bq_content, :class => 'quotes'
+          bq.content.should    == @bq_content
+          bq.title.should      == @bq_title
+          bq.attributes.should == {:cites => @bq_attributes[:cites], :class => [@bq_title.hyphenize, 'quotes']}
+        end
+      end
+
     end
   end
 
@@ -32,13 +131,13 @@ describe ActiveComponent::Base do
       children.should == [b.object_id, c.object_id]
     end
   end
-  
+
   context "instantiation helpers" do
     it "should be available after creating a component" do
       class NewGreetingBox < ActiveComponent::Base; end
       ActiveComponent.instance_methods.should be_include('new_greeting_box')
     end
-    
+
     # TODO: How to test this?
     it "should be available to ActionView and render output"
     #   class GreetingBox < ActiveComponent::Base
@@ -48,5 +147,6 @@ describe ActiveComponent::Base do
     #   ActiveComponent::TemplateHandler.new.instance_eval { compile(greeting_box("world")) }.should == "not specified yet"
     # end
   end
-  
+
 end
+
--- spec/components/block_spec.rb
+++ spec/components/block_spec.rb
@@ -3,7 +3,7 @@ describe Block do
   include ActiveComponent
   include Haml::Helpers
   include ActiveSupport::CoreExtensions::String::Inflections
-  
+
   before :each do
     @content     = "It is better to be quotable than to be honest."
     @title       = "Tom Stoppard on Quotations"
@@ -12,102 +12,25 @@ describe Block do
   end
 
   describe "initialize" do
-      
-    it "should set all parameters correctly when given in order" do
-      block = Block.new @content, @title, @tag_type, @attributes
-      block.content.should            == @content
-      block.title.should              == @title
-      block.tag_type.should           == @tag_type
-      block.attributes[:cites].should == @attributes[:cites]
-    end
-    
-    it "should set all parameters correctly when given as key value pairs" do
-      block = Block.new :attributes => @attributes, :title => @title, :content => @content, :tag_type => @tag_type
-      block.content.should    == @content
-      block.title.should      == @title
-      block.tag_type.should   == @tag_type
-      block.attributes.should == @attributes
-    end
-    
-    it "should set all parameters correctly when given in a mixed way" do
-      block = Block.new @title, @tag_type, :content => @content, :attributes => @attributes
-      block.content.should    == @content
-      block.title.should      == @title
-      block.tag_type.should   == @tag_type
-      block.attributes.should == @attributes
-    end
-    
     it "should assume :div as default block type" do
       block = Block.new { @content }
       block.tag_type.should == :div
     end
-      
-    it "should prioritize content blocks over arguments" do
-      block = Block.new(:content => "ignorable") { @content }
-      block.content.should == @content
-    end
-    
-    it "should fill @attributes with remaining arguments of the last hash" do
-      block = Block.new @title, :cites => "http://www.quotationspage.com/quote/368.html", :content => @content, :class => 'quotes'
-      block.content.should    == @content
-      block.title.should      == @title
-      block.attributes.should == {:cites => @attributes[:cites], :class => [@title.hyphenize, 'block', 'quotes']}
-    end
-    
-    context "subclasses" do
+
+    it "should not be possible to overwrite tag_type for subclasses of Block" do
       for elem in ActiveComponent::BLOCK_ELEMENTS
         elem_class = elem.to_s.camelize.constantize
-        
-        it "should set all parameters correctly when given in order" do
-          block = elem_class.new @content, @title, @attributes
-          block.content.should    == @content
-          block.title.should      == @title
-          block.tag_type.should   == elem
-          block.attributes.should == {:cites => @attributes[:cites], :class => [@title.hyphenize, 'quotes']}
-        end
-        
-        it "should set all parameters correctly when given as key value pairs" do
-          block = elem_class.new :attributes => @attributes, :title => @title, :content => @content
-          block.content.should    == @content
-          block.title.should      == @title
-          block.tag_type.should   == elem
-          block.attributes.should == {:cites => @attributes[:cites], :class => [@title.hyphenize, 'quotes']}
-        end
-        
-        it "should set all parameters correctly when given in a mixed way" do
-          block = elem_class.new @title, :content => @content, :attributes => @attributes
-          block.content.should    == @content
-          block.title.should      == @title
-          block.tag_type.should   == elem
-          block.attributes.should == {:cites => @attributes[:cites], :class => [@title.hyphenize, 'quotes']}
-        end
-        
-        it "should prioritize content blocks over arguments" do
-          block = elem_class.new(:content => "ignorable") { @content }
-          block.content.should == @content
-        end
-        
-        it "should not be possible to overwrite tag_type for subclasses of Block" do
-          block = elem_class.new(:tag_type => :block) { @content }
-          block.tag_type.should == elem
-        end
-        
-        it "should fill @attributes with remaining arguments of the last hash" do
-          block = elem_class.new @title, :cites => "http://www.quotationspage.com/quote/368.html", :content => @content, :class => 'quotes'
-          block.content.should    == @content
-          block.title.should      == @title
-          block.attributes.should == {:cites => @attributes[:cites], :class => [@title.hyphenize, 'quotes']}
-        end
+        block = elem_class.new(:tag_type => :block) { @content }
+        block.tag_type.should == elem
       end
     end
-    
   end
-  
+
   describe "to_html" do
-    it "should render nested blocks" do       
+    it "should render nested blocks" do
       init_buffer
       block = Block.new @title, @tag_type, @attributes do
-        [ 
+        [
           Block.new(@title, :tag_type => :h1),
           Block.new("It is better to be quotable than to be honest.", :tag_type => :p),
           Block.new("Details", :tag_type => :aside) do
@@ -123,6 +46,6 @@ describe Block do
       block.to_html.should == "<#{@tag_type} cites='#{@attributes[:cites]}' class='#{@title.hyphenize} block #{@attributes[:class]}'>\n  <h1 class='block'>\n    #{@title}\n  </h1>\n  <p class='block'>\n    #{@content}\n  </p>\n  <aside class='details block'>\n    <dl>\n      <dt>Author</dt>\n      <dd>Tom Stoppard</dd>\n    </dl>\n    \n  </aside>\n  \n</#{@tag_type}>\n"
     end
   end
-  
+
 end
 
